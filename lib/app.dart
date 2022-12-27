c8c8b409fcf4ba056c4691362151c9c12233d25d
Bifi <vin1c1u92@gmail.com>
2022-12-27 13:04:11 -0300

    Ajuste para manter o usuario logado

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hub_ideias/utils/constants/constants.dart';
 import 'package:flutter_hub_ideias/routes/route.dart';
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: R_LOGIN,
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? R_LOGIN : R_DASHBOARD,
       routes: rotes,
     );
   }
